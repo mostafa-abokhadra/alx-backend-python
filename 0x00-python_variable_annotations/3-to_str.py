6d99fe705c96255d77201d302bf4318c219bf2ff
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-01 22:42:21 +0300

    task 4

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""converting float to string"""
+
+
+def to_str(n: float) -> str:
+    """
+    Args:
+        n: float number
+    Returns:
+        string represntation of float num: str
+    """
+    return str(n)
