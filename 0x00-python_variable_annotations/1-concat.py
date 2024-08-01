5ea0215e3c81d0d0861e27f462247bb61bbedc55
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-01 22:35:40 +0300

    task 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""concatinating two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Args:
+        str1: str
+        str2: str
+    Returns:
+        concat string: str
+    """
+    return str1 + str2
