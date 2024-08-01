33c152f1ff4e285357703a6bf181068cd735bf00
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-01 22:54:24 +0300

    adding task6 func doc

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,6 +4,12 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """
+    Args:
+        input_list: list of float nums
+    Returns:
+        the sum of list as: float
+    """
     sum: float = 0.0
     for i in input_list:
         sum += i
