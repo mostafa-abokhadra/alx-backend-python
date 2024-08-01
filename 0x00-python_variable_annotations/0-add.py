48598d025a885c16a36efb5c91b1afb0859b1cc4
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-01 22:33:12 +0300

    adding docs

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,2 +1,13 @@
+#!/usr/bin/env python3
+"""adding 2 floats module"""
+
+
 def add(a: float, b: float) -> float:
+    """
+    Args:
+        a: float number
+        b: float number
+    Returns:
+        sum of a and b: float
+    """
     return a + b
