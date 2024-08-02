d283a808d61ec8a8d6ef2ecf79bb87249d808b31
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-02 04:10:02 +0300

    101

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,4 +1,15 @@
-def safely_get_value(dct, key, default = None):
+#!/usr/bin/env python3
+"""Advanced type"""
+from typing import Any, Mapping, TypeVar, Union
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]
+                     = None) -> Union[Any, T]:
+    """
+    Args:
+    Returns:
+    """
     if key in dct:
         return dct[key]
     else:
