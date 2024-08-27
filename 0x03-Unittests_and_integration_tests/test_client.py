648569d4524b7527306ab8a8f66815ae92641734
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-27 15:02:22 +0300

    fix

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -14,16 +14,18 @@ class TestGithubOrgClient(unittest.TestCase):
     @parameterized.expand(
             [
                 "google",
-                "google"
+                "abc"
             ]
     )
     @patch('client.get_json')
     def test_org(self, org, mock_get_json):
         """testing organization
         """
+        res = {"name": "mostafa abokhadra"}
         mock_get_json.return_value = {"name": "mostafa abokhadra"}
         inst = GithubOrgClient(org)
         val = inst.org
         val = inst.org
-        # self.assertIsInstance(val, dict)
+        self.assertIsInstance(val, dict)
+        self.assertEqual(val, res)
         mock_get_json.assert_called_once()
