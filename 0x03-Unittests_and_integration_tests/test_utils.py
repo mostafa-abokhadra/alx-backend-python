a7c5653b493a49d1d8c3be050807cc79959fde57
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-27 13:41:49 +0300

    fixing styling

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -27,6 +27,8 @@ class TestAccessNestedMap(unittest.TestCase):
         ]
     )
     def test_access_nested_map_exception(self, nested_map, path):
+        """test_access_nested_map_exception
+        """
         self.assertRaises(KeyError, access_nested_map, nested_map, path)
 
 
